75c6e2e0b917e76c9d34fd3c21c20e009628a3bd
Mathis Dröge <admin@commandmc.de>
2021-05-11 09:29:00 +0200

    Add pinMode() to Motor functions
    This prevents issues with analogWrite and digitalWrite being used together
    See https://forum.arduino.cc/t/analogwrite-incompatable-with-digitalwrite/152713/33
    for details

--- Motor/src/Motor.cpp
+++ Motor/src/Motor.cpp
@@ -12,20 +12,26 @@ void Motor::setMotorPower(int pwm) {
     if (pwm > 0) {
       // Vorwärts fahren
       analogWrite(_anschlussVor, pwm);
+      pinMode(_anschlussRueck, OUTPUT);
       digitalWrite(_anschlussRueck, LOW);
     } else {
       // Rückwärts fahren
+      pinMode(_anschlussVor, OUTPUT);
       digitalWrite(_anschlussVor, LOW);
       analogWrite(_anschlussRueck, abs(pwm));
     }
   } else {
     // Bremsen
+    pinMode(_anschlussVor, OUTPUT);
+    pinMode(_anschlussRueck, OUTPUT);
     digitalWrite(_anschlussVor, HIGH);
     digitalWrite(_anschlussRueck, HIGH);
   }
 }
 
 void Motor::leerlauf() {
+  pinMode(_anschlussVor, OUTPUT);
+  pinMode(_anschlussRueck, OUTPUT);
   digitalWrite(_anschlussVor, LOW);
   digitalWrite(_anschlussRueck, LOW);
 }
